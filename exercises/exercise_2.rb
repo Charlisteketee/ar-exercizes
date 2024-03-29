22ae658b9c09ae3a508a1bf59cc4d8c525040571
Charlisteketee <charlisteketee@gmail.com>
2024-03-28 20:12:02 -0700

    Exercize 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+#find method is used to retrieve data
+
+@store1 = Store.find_by(id: 1)
+
+@store2 = Store.find_by(id: 2)
+
+store = Store.find_by(id: 1)
+store.update(name: "Victoria")
+
+
+
+
