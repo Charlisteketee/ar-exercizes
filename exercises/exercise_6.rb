fcfa393303e91ad5f0cc65783e18987680e637a8
Charlisteketee <charlisteketee@gmail.com>
2024-03-28 21:15:51 -0700

    Exercize 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Char", last_name: "Poncho", hourly_rate: 70)
+@store1.employees.create(first_name: "Charmander", last_name: "Ponchomaker", hourly_rate: 90)
+
+@store2.employees.create(first_name: "Shelly", last_name: "Harold", hourly_rate: 90)
+@store2.employees.create(first_name: "Kurt", last_name: "Johnson", hourly_rate: 60)
+@store2.employees.create(first_name: "Russell", last_name: "Williams", hourly_rate: 80)
